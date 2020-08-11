1f25792f0dc1256ef1491e3addbeffa63cb123d5
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-10 23:30:25 -0300

    Select Component

---
+++ web/src/components/Select/index.tsx
@@ -0,0 +1,50 @@
+import React, { SelectHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+    name: string;
+    label: string;
+    options: Array<{ value: string, label: string }>;
+}
+
+const Materias = [
+    { value: 'Artes', label: 'Artes' },
+    { value: 'Biologia', label: 'Biologia' },
+    { value: 'Ciências', label: 'Ciências' },
+    { value: 'Educação Física', label: 'Educação Física' },
+    { value: 'Física', label: 'Física' },
+    { value: 'Geografia', label: 'Geografia' },
+    { value: 'História', label: 'História' },
+    { value: 'Matemática', label: 'Matemática' },
+    { value: 'Português', label: 'Português' },
+    { value: 'Química', label: 'Química' }
+];
+
+const WeekDay = [
+    { value: '0', label: 'Domingo' },
+    { value: '1', label: 'Segunda-feira' },
+    { value: '2', label: 'Terça-feira' },
+    { value: '3', label: 'Quarta-feira' },
+    { value: '4', label: 'Quinta-feira' },
+    { value: '5', label: 'Sexta-feira' },
+    { value: '6', label: 'Sábado' },
+];
+
+const Select: React.FunctionComponent<SelectProps> = ({ label, name, options, ...rest }) => {
+    return (
+        <div className="select-block">
+            <label htmlFor={name}> {label} </label>
+
+            <select id={name} {...rest} defaultValue={"disabled"}>
+                <option value="disabled" hidden>Selecione uma opção</option>
+
+                {options.map((option) => {
+                    return <option key={option.value} value={option.value}>{option.label}</option>;
+                })}
+            </select>
+        </div>
+    );
+}
+
+export { Select, Materias, WeekDay };
