acea36b65280311658cc1dfad181f548ee474d11
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-13 04:46:20 -0300

    PageHeader

--- mobile/src/pages/PageHeader/index.tsx
+++ mobile/src/pages/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler'
 
@@ -10,9 +10,10 @@ import { useNavigation } from '@react-navigation/native';
 
 interface PagerHeaderProps {
     title: string;
+    headerRight?: ReactNode
 }
 
-const PageHeader: React.FunctionComponent<PagerHeaderProps> = ({ title }) => {
+const PageHeader: React.FunctionComponent<PagerHeaderProps> = ({ title, headerRight, children }) => {
     const { navigate } = useNavigation();
 
     function handleGoBack() {
@@ -27,9 +28,13 @@ const PageHeader: React.FunctionComponent<PagerHeaderProps> = ({ title }) => {
 
             <Image source={logoImg} resizeMode="contain" />
         </View>
-        <Text style={styles.title}>
-            {title}
-        </Text>
+
+        <View style={styles.header}>
+            <Text style={styles.title}>{title}</Text>
+            {headerRight}
+        </View>
+
+        {children}
     </View>
 }
 
