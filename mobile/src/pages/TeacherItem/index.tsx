1d9b729986572db35915ff9157f3dffa2438a714
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-13 20:19:53 -0300

    TeacherItem

--- mobile/src/pages/TeacherItem/index.tsx
+++ mobile/src/pages/TeacherItem/index.tsx
@@ -9,6 +9,7 @@ import { RectButton } from 'react-native-gesture-handler';
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -30,7 +31,11 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
     const [isFavorited, setIsFavorited] = useState(favorited);
 
     function handleLinkToWhatsapp() {
-        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
+        api.post('/connections', {
+            user_id: teacher.id
+        });
+
+        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
     }
 
     async function handleToogleFavorite() {
