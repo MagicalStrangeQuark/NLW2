b63312f8512d734ca4c35766ae9a481ce48321b3
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-13 06:48:14 -0300

    Added Dynamic Values

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -4,14 +4,32 @@ import { Feather } from '@expo/vector-icons';
 
 import styles from './styles';
 import PageHeader from '../PageHeader';
-import TeacherItem from '../TeacherItem';
+import TeacherItem, { Teacher } from '../TeacherItem';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 function TeacherList() {
+    const [teachers, setTeachers] = useState([]);
     const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+    const [week_day, setWeekDay] = useState('');
+    const [subject, setSubject] = useState('');
+    const [time, setTime] = useState('');
 
     function handleToogleFiltersVisible() {
         setIsFiltersVisible(!isFiltersVisible);
+    }
+
+    async function handleFiltersSubmit() {
+        const response = await api.get('/classes', {
+            params: {
+                week_day: week_day,
+                subject: subject,
+                time: time
+            }
+        });
+
+        setTeachers(response.data);
     }
 
     return (
@@ -23,19 +41,32 @@ function TeacherList() {
                 {isFiltersVisible && (
                     <View style={styles.searchForm}>
                         <Text style={styles.label}>Matéria</Text>
-                        <TextInput style={styles.input} placeholderTextColor="#C1BCCC" placeholder="Qual a matéria?" />
-
+                        <TextInput
+                            style={styles.input}
+                            placeholderTextColor="#C1BCCC"
+                            placeholder="Qual a matéria?"
+                            value={subject}
+                            onChangeText={text => setSubject(text)} />
                         <View style={styles.inputGroup}>
                             <View style={styles.inputBlock}>
                                 <Text style={styles.label}>Dia da Semana</Text>
-                                <TextInput style={styles.input} placeholderTextColor="#C1BCCC" placeholder="Qual o dia?" />
+                                <TextInput
+                                    style={styles.input}
+                                    placeholderTextColor="#C1BCCC"
+                                    placeholder="Qual o dia?"
+                                    value={week_day}
+                                    onChangeText={text => setWeekDay(text)} />
                             </View>
                             <View style={styles.inputBlock}>
                                 <Text style={styles.label}>Horário</Text>
-                                <TextInput style={styles.input} placeholderTextColor="#C1BCCC" placeholder="Qual horário?" />
+                                <TextInput style={styles.input}
+                                    placeholderTextColor="#C1BCCC"
+                                    placeholder="Qual horário?"
+                                    value={time}
+                                    onChangeText={text => setTime(text)} />
                             </View>
                         </View>
-                        <RectButton style={styles.submitbutton}>
+                        <RectButton onPress={handleFiltersSubmit} style={styles.submitbutton}>
                             <Text style={styles.submitButtonText}>Filtrar</Text>
                         </RectButton>
                     </View>
@@ -46,13 +77,10 @@ function TeacherList() {
                 paddingHorizontal: 16,
                 paddingBottom: 16
             }}>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+
+                {
+                    teachers.map((teacher: Teacher) => <TeacherItem key={teacher.id} teacher={teacher} />)
+                }
             </ScrollView>
         </View>
     );
