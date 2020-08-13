8b44eeab29546e086e18546df61026157799e02c
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-13 04:46:01 -0300

    Favorites

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import styles from './styles';
 import PageHeader from '../PageHeader';
+import TeacherItem from '../TeacherItem';
 
 function Favorites() {
     return (
         <View style={styles.container}>
             <PageHeader title="Meus Proffys Favoritos" />
+
+            <ScrollView style={styles.teacherList} contentContainerStyle={{
+                paddingHorizontal: 16,
+                paddingBottom: 16
+            }}>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </ScrollView>
         </View>
     );
 }
